b9fa1ec1238bf34efdb934cbcd82f97a64042fe4
fasyaais <dyqdvo@gmail.com>
2026-02-25 16:34:01 +0700

    add: heap usage

--- 03-memory-and-crash-handling/main/main.cpp
+++ 03-memory-and-crash-handling/main/main.cpp
@@ -1,33 +1,54 @@
 #include "freertos/FreeRTOS.h"
 #include "esp_log.h"
 #include "driver/gpio.h"
-#include <cstdint>
+#include "esp_system.h"
+
+#define MAP_DRAM_TO_IRAM(addr) (addr)
 
 void load_fault();
 void stack_overflow(void *pvParameters);
 void instr_fetch();
 void store_proh();
+void heapTask(void *args);
 
 extern "C" {
     void app_main();
 }
 
-
 void app_main(void)
 {
-    // xTaskCreate(
-    //     stack_overflow,
-    //     "LED blink",
-    //     512,
-    //     NULL,
-    //     3,
-    //     NULL
-    // );
+    xTaskCreate(
+        stack_overflow,
+        "LED blink",
+        1024,
+        NULL,
+        3,
+        NULL
+    );
+    xTaskCreate(heapTask,"Memory Monitor",2048,nullptr,3,nullptr);
 
     // load_fault();
     // illegal_inst();
     // store_proh();
-    instr_fetch();
+    // instr_fetch();
+    // ilegal_instr();
+
+    void *ptr = malloc(5120);
+}
+
+void heapTask(void *args)
+{
+    static const char* TAG = "MEM_MONITOR";
+    while (true)
+    {
+        uint32_t free_heap = esp_get_free_heap_size();
+        uint32_t minimum_free_heap = esp_get_minimum_free_heap_size(); 
+        ESP_LOGI(TAG,"-----------------");
+        ESP_LOGI(TAG,"Free heap     : %lu",free_heap);
+        ESP_LOGI(TAG,"Min Free Heap : %lu",minimum_free_heap);
+        vTaskDelay(pdMS_TO_TICKS(5000));
+    }
+    
 }
 
 void stack_overflow(void *pvParameters)
@@ -41,7 +62,6 @@ void stack_overflow(void *pvParameters)
     while (true)
     {
         if((xTaskGetTickCount() - ledTick) > (ledPeriod/2)){
-            printf("debug\n");
             stateLED = !stateLED;
             gpio_set_level(led_pin, stateLED);
             ledTick = xTaskGetTickCount();
